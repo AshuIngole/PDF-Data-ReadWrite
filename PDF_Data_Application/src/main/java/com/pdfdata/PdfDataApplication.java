eeb101c49729458106c5bcd505762d667416a076
Admin <Admin@DESKTOP-QMFU2H6>
2023-12-08 16:42:45 +0530

    test PDF Data Read Write

---
+++ PDF_Data_Application/src/main/java/com/pdfdata/PdfDataApplication.java
@@ -0,0 +1,13 @@
+package com.pdfdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfDataApplication.class, args);
+	}
+
+}
---
+++ PDF_Data_Application/src/test/java/com/pdfdata/PdfDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
